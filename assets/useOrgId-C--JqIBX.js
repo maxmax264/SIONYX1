933501e0e3293edcd7b2fa30c6196c32c8745c35
maxmax264 <hadasa004000@gmail.com>
2026-05-20 11:57:07 +0300

    Updates

---
+++ assets/useOrgId-C--JqIBX.js
@@ -0,0 +1,1 @@
+import{u as t}from"./index-Fs1pzHoe.js";const g=()=>t(r=>r.getOrgId()),s=()=>t.getState().getOrgId();export{s as g,g as u};
--- assets/useOrgId-kDu2HB7B.js
+++
@@ -1,1 +0,0 @@
-import{u as t}from"./index-DxEyD-i0.js";const g=()=>t(r=>r.getOrgId()),s=()=>t.getState().getOrgId();export{s as g,g as u};
