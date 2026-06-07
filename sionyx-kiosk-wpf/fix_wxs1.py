abb41a80d0daa7124a9486f4c36c1f2759904cf2
maxmax264 <hadasa004000@gmail.com>
2026-06-07 19:52:26 +0300

    add SetupFirstLogon - AutoLogon once for SionyxUser profile creation

---
+++ sionyx-kiosk-wpf/fix_wxs1.py
@@ -0,0 +1,15 @@
+﻿content = open(r'.\installer\Package.wxs', encoding='utf-8').read()
+
+old = '<CustomAction Id="CA_VerifyInstallation"'
+new = '''<CustomAction Id="CA_SetupFirstLogon" BinaryRef="CustomActionsDll" DllEntry="SetupFirstLogon" Execute="deferred" Impersonate="no" Return="check" />
+    <SetProperty Id="CA_SetupFirstLogon" Before="CA_SetupFirstLogon" Sequence="execute" Value="INSTALLDIR=[INSTALLFOLDER]" />
+    <CustomAction Id="CA_VerifyInstallation"'''
+
+count = content.count(old)
+print(f"Found {count} matches")
+if count == 1:
+    content = content.replace(old, new, 1)
+    open(r'.\installer\Package.wxs', 'w', encoding='utf-8').write(content)
+    print('OK')
+else:
+    print('NOT FOUND')
