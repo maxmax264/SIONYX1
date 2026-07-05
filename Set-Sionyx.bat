c4cc8ae716d70b0fa9ebf266a2641584f345d69e
maxmax264 <hadasa004000@gmail.com>
2026-07-05 20:39:30 +0300

    fix: always use the Nedarim-confirmed iframe ApiValid (QWtE4M6uVn), not just when save-card is checked

---
+++ Set-Sionyx.bat
@@ -0,0 +1,6 @@
+@echo off
+git config user.name "maxmax264"
+git config user.email "hadasa004000@gmail.com"
+git remote set-url origin git@github.com-hadasa:maxmax264/SIONYX1.git
+echo --- המערכת הוגדרה לפרופיל SIONYX (maxmax264) ---
+pause
