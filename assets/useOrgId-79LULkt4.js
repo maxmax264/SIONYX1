fcdba65b56df351ea55121042a7e786da07b01d6
maxmax264 <hadasa004000@gmail.com>
2026-09-11 15:03:01 +0300

    Updates

---
+++ assets/useOrgId-79LULkt4.js
@@ -0,0 +1,1 @@
+import{u as t}from"./index-DmkpDVVB.js";const g=()=>t(r=>r.getOrgId()),s=()=>t.getState().getOrgId();export{s as g,g as u};
--- assets/useOrgId-Dse2otEY.js
+++
@@ -1,1 +0,0 @@
-import{u as t}from"./index-C_VRmLsg.js";const g=()=>t(r=>r.getOrgId()),s=()=>t.getState().getOrgId();export{s as g,g as u};
